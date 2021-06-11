fa465b6806755387df15dbdf0ebc011038180e04
jhs512 <jangka512@gmail.com>
2021-06-11 11:18:44 +0900

    MaterialTheme, Text 꾸미기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -47,22 +49,40 @@ fun NewsStory() {
     }
     */
 
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.header),
-            contentDescription = null,
+    MaterialTheme {
+        Column(
             modifier = Modifier
-                .height(180.dp)
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
 
-        Text(text = "A day in Shark Fin Cove")
-        Text(text = "Davenport, California")
-        Text(text = "December 2018")
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = "A day wandering through the sandhills" +
+                        " in Shark Fin Cove, and a few of the" +
+                        " sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                text = "December 2018",
+                style = typography.body2
+            )
+        }
     }
 }
 
