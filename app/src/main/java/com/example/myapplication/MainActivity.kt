fe6a64c362e82af6c83b015e7deffa7b6037af2d
jhs512 <jangka512@gmail.com>
2021-06-11 10:57:45 +0900

    column

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,18 +12,42 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Gretting("Android")
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Gretting(name: String) {
-    Text(text = "Hello, $name")
+fun NewsStory() {
+    /*
+    Column(modifier = Modifier.background(Color.Red), content = {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    })
+    */
+
+    /*
+    Column(
+        modifier = Modifier
+            .background(Color.Red)
+            .fillMaxWidth()
+    ) {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
+    */
+
+    Column {
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
 }
 
 @Preview
 @Composable
-fun PreviewGretting() {
-    Gretting("Android")
+fun PreviewApp() {
+    NewsStory()
 }
